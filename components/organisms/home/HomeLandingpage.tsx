84bba5dcb8bb848af4f3e970cf9da792ce39a7ed
princeofverry <verrykurniawan@students.undip.ac.id>
2025-01-17 08:46:47 +0700

    add: content h1 and p

---
+++ components/organisms/home/HomeLandingpage.tsx
@@ -0,0 +1,15 @@
+export default function HomeLandingpage() {
+  return (
+    <>
+      <section>
+        <div className="text-white flex flex-col items-center justify-center w-screen h-screen">
+          <h1>I design and build clean websites</h1>
+          <p>
+            Write anything here something about yourself to showcase what
+            actually you doing or targeting etc.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ components/organisms/home/HomeWrapper.tsx
@@ -0,0 +1,11 @@
+import HomeLandingpage from "./HomeLandingpage";
+
+export default function HomeWrapper() {
+  return (
+    <>
+      <section>
+        <HomeLandingpage />
+      </section>
+    </>
+  );
+}
