390dbd07fa9d490ddcddb274dd36e3263130b790
princeofverry <verrykurniawan@students.undip.ac.id>
2025-01-17 13:13:33 +0700

    add: project

--- components/organisms/home/HomeWrapper.tsx
+++ components/organisms/home/HomeWrapper.tsx
@@ -1,6 +1,7 @@
 import HomeAward from "./HomeAward";
 import HomeLandingpage from "./HomeLandingpage";
 import HomeMarquee from "./HomeMarquee";
+import HomeProject from "./HomeProject";
 
 export default function HomeWrapper() {
   return (
@@ -9,6 +10,7 @@ export default function HomeWrapper() {
         <HomeLandingpage />
         <HomeMarquee />
         <HomeAward />
+        <HomeProject />
       </section>
     </>
   );
