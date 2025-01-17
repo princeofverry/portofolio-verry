491f830e08d5d8f287cfe62443f9c095d803ca0b
princeofverry <verrykurniawan@students.undip.ac.id>
2025-01-17 17:15:49 +0700

    add: project

--- data/data.ts
+++ data/data.ts
@@ -27,3 +27,37 @@ export const Experiece = [
       "I am part of the Machine Learning track at Bangkit Academy, where I focus on developing expertise in applying machine learning techniques to real-world problems. Through this program, I gain hands-on experience with tools and algorithms that power modern AI systems, preparing me for a career in this rapidly growing field. The training emphasizes practical knowledge, from model development to deployment, giving me a strong foundation in machine learning technologies.",
   },
 ];
+
+export const Project = [
+  {
+    id: 1,
+    title: "Dopamind+",
+    image: "/images/dopamind.png",
+    description: "lorem ipsum dolor sit amet, consectetur adipis...",
+  },
+  {
+    id: 2,
+    title: "MindfulMe",
+    image: "/images/bangkit.png",
+    description: "Explore mindfulness techniques for better mental health.",
+  },
+  {
+    id: 3,
+    title: "FocusFlow",
+    image: "/images/bangkit.png",
+    description: "A productivity app to help you stay focused and organized.",
+  },
+  {
+    id: 4,
+    title: "HappyHabits",
+    image: "/images/bangkit.png",
+    description: "Track your habits and improve your well-being over time.",
+  },
+  {
+    id: 5,
+    title: "ZenSpace",
+    image: "/images/bangkit.png",
+    description:
+      "A virtual space to practice relaxation techniques and meditate.",
+  },
+];
